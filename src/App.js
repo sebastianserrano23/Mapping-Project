d0252cbecb2d9df4c2ea80cfca040905827dc477
Sebastian Serrano <serranosebastian23@gmail.com>
2023-03-21 14:51:59 -0700

    pushing mapping project into a github repository

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import MappingVisualizer from './MappingVisualizer/MappingVisualizer';
+
+function App() {
+  return (
+    <div className="App">
+      <MappingVisualizer></MappingVisualizer>
+    </div>
+  );
+}
+
+export default App;
